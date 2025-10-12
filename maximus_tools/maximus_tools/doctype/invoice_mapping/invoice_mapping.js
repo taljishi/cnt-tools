03a46166d9485a3208f30e10a61a758ec311db51
taljishi <27351210+taljishi@users.noreply.github.com>
2025-10-12 08:22:52 +0000

    chore: remove invoice run + mapping doctypes from main (kept on feature branch)

--- maximus_tools/maximus_tools/doctype/invoice_mapping/invoice_mapping.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Cloud Nine Technologies (CNT) and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Invoice Mapping', {
-	// refresh: function(frm) {
-
-	// }
-});
