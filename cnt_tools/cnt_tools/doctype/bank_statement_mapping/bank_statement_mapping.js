eeed98f8c1c2fb8b781e47b791ba93555b11872d
taljishi <27351210+taljishi@users.noreply.github.com>
2025-10-18 17:07:18 +0000

    chore: align doctypes with new modules (CNT HR, CNT Accounting); workspace/hooks updated

--- cnt_tools/cnt_tools/doctype/bank_statement_mapping/bank_statement_mapping.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Cloud Nine Technologies (CNT) and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Bank Statement Mapping', {
-	// refresh: function(frm) {
-
-	// }
-});
