18f0bf2fab99015709c3f2198921e2dbbd1b102e
taljishi <27351210+taljishi@users.noreply.github.com>
2025-10-18 09:22:39 +0000

    chore(fleet): move Fuel Card/Consumption doctypes under cnt_tools.fleet

---
+++ cnt_tools/fleet/doctype/fuel_card/test_fuel_card.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Cloud Nine Technologies and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestFuelCard(FrappeTestCase):
+	pass
---
+++ cnt_tools/fleet/doctype/fuel_consumption/fuel_consumption.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Cloud Nine Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Fuel Consumption', {
+	// refresh: function(frm) {
+
+	// }
+});
